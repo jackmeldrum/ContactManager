521282b5cac12a35a63d447da6736047724de534
Tyler Rothenberg <rothenbergt@gmail.com>
2019-09-14 13:00:24 -0400

    Create MySqlCon.java
    Test .Java file for connecting to the database from within the sever

---
+++ API/MySqlCon.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+class MySqlCon
+{
+
+	public static void main(String args[])
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+
+			Connection con = DriverManager.getConnection(
+			                   "jdbc:mysql://localhost:3306/contact_manager",
+                               "username",
+                               "password");
+
+			System.out.println("We have connected");
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
